f20ce59760bb1bb5381f118f59c8585f8ad8b8e7
tomoho <40868553+tomoho@users.noreply.github.com>
2018-09-26 15:25:28 -0400

    Update conf.py

--- myproject/track/conf.py
+++ myproject/track/conf.py
@@ -8,6 +8,7 @@ logging.basicConfig(filename='logger.log',level=logging.CRITICAL)
 Version     ='0.0'
 Date    ='20180829'
 
+Universial_milestone =['订单创建','包裹出库','航班起飞','正在清关','国内配送']
 milestone={
-          '天马'=:['订单创建','包裹入库']
+          '天马':['订单创建','包裹入库']
 }

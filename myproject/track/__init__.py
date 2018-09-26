4d74c3418160b6c82f3437429bd0d7650f3fd536
tomoho <40868553+tomoho@users.noreply.github.com>
2018-09-26 14:33:00 -0400

    Update __init__.py

--- myproject/track/__init__.py
+++ myproject/track/__init__.py
@@ -1,1 +1,39 @@
 
+'''
+使用时直接导入模块即可运行命令
+ex.
+   import tianmatrack
+   track('trackingid')   will return current status and history
+
+or
+    import tianmatrack
+    x=new_instance
+    x.online_tracking('trackingid') will return current status and history
+'''
+
+
+from .tianmatrack import *
+
+
+
+instanceList    =   []
+
+def new_instance():
+    newInstance     =   Tracking()
+    instanceList.append(newInstance)
+    return newInstance
+
+originInstance =    new_instance()
+
+track           =   originInstance.track_order
+
+'''
+        result = {'stepdetail':stepdetail,
+                  'transdetail':transdetail,
+                  'citys':citys,
+                  'carrier':carrier,
+                  'month':month,
+                  'duration':duration}
+        return result
+'''
+

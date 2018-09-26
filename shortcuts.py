2bf224d6831a7b8c543383c3aecf4cd1fead3122
tomoho <40868553+tomoho@users.noreply.github.com>
2018-09-26 09:51:22 -0400

    Delete shortcuts.py

--- shortcuts.py
+++
@@ -1,6 +0,0 @@
-"""
-This module collects helper functions and classes that "span" multiple levels
-of MVC. In other words, these functions/classes introduce controlled coupling
-for convenience's sake.
-"""
-import warnings

1bd7f2c7143371743a64433e96d2e581e2f26320
tomoho <40868553+tomoho@users.noreply.github.com>
2018-09-26 09:48:00 -0400

    Add files via upload

---
+++ shortcuts.py
@@ -0,0 +1,6 @@
+"""
+This module collects helper functions and classes that "span" multiple levels
+of MVC. In other words, these functions/classes introduce controlled coupling
+for convenience's sake.
+"""
+import warnings

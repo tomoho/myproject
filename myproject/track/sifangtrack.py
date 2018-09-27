3d7980eeb7ce8c904f075f8100bfe51e8f435a22
tomoho <40868553+tomoho@users.noreply.github.com>
2018-09-27 11:33:26 -0400

    Update sifangtrack.py

--- myproject/track/sifangtrack.py
+++ myproject/track/sifangtrack.py
@@ -16,4 +16,41 @@ HTML=html.read().decode('utf8')
 
 result=json.loads(HTML)
 
-history=result.get['data']
+details=result.get('data')             # details is a list with len==1
+details=details[0]                    # details is dict 
+EndDeliverySupplierName=details.get('EndDeliverySupplierName')
+TrackingNumber=details.get('TrackingNumber')
+ReceiverID=details.get('ReceiverID')
+ReferenceNumber=details.get('ReferenceNumber')
+
+'''['ItemID', 'ReferenceNumber', 'TrackingNumber', 'EndDeliverySupplierCode',
+'ChannelCode', 'ReceiverID', 'SourceCode', 'TraceSourceNumber', 
+'EndDeliverySupplierName', 'is_use_idCode', 'tracks']
+'''
+history=details.get('tracks')         # history is a list with length based on result 
+                                      # each item is dict with key 
+'''['TrackID', 'ItemID', 'TrackStatusCode', 'TrackDate', 'TrackLocation', 
+'TrackContent', 'CreateDate', 'UserIDCreate', 'SyncFPX']
+
+TrackID 29773742
+ItemID 1478963
+TrackStatusCode FT
+TrackDate 2018-09-22 02:24:51
+TrackLocation 芝加哥
+TrackContent 起飞
+CreateDate 2018-09-22 02:28:41
+UserIDCreate 1588
+SyncFPX 1
+'''
+                   
+return {
+        'receiver':ReceiverID,
+        'ReferenceNumber':ReferenceNumber,
+        'EndDeliverySupplierName':EndDeliverySupplierName,
+        'TrackingNumber':TrackingNumber,
+        'history':history        
+}
+                   
+                   
+
+

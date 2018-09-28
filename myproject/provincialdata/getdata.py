6fdcfc53ab04d8a1bfc30966168f925af1c8d98c
tomoho <40868553+tomoho@users.noreply.github.com>
2018-09-28 11:40:46 -0400

    Update getdata.py

--- myproject/provincialdata/getdata.py
+++ myproject/provincialdata/getdata.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import urllib.request
-import re
+import urllib.request as request
+import urllib.parse as parse
+import json
 from datetime import datetime
 
 url= http://www.ems.com.cn/serviceguide/you_bian_cha_xun.html

a7ec9aa73842be01dfeb03ad7633633c0c001401
tomoho <40868553+tomoho@users.noreply.github.com>
2018-09-28 11:40:08 -0400

    Update getdata.py

--- myproject/provincialdata/getdata.py
+++ myproject/provincialdata/getdata.py
@@ -5,3 +5,28 @@ import re
 from datetime import datetime
 
 url= http://www.ems.com.cn/serviceguide/you_bian_cha_xun.html
+  
+country=http://www.ems.com.cn/ems/tool/rpq/countries  
+{"model":{"selectedCountry":[{"countryName":"TNT公司","countryCode":"WW"},....
+                            ]
+         },
+ "cleard":false
+}
+provience = http://www.ems.com.cn/ems/tool/rpq/provinces
+{"model":{"selectedProv":[{"provName":"河北","provCode":"13"},...
+                         ]
+         },
+ "cleared":false
+}
+city=http://www.ems.com.cn/ems/tool/rpq/cities?province=11
+
+{"model":{"selectedCity":[{"cityName":"安丘市","cityCode":"370784"},....
+                         ]
+         },
+ "cleared":false
+}  
+  
+'''
+return data type :
+
+'''

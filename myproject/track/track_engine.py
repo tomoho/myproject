1ffed3d2ea8326c7448c16e6cbef60d411ea62a6
tomoho <40868553+tomoho@users.noreply.github.com>
2018-10-01 11:08:05 -0400

    Update track_engine.py

--- myproject/track/track_engine.py
+++ myproject/track/track_engine.py
@@ -81,8 +81,9 @@ class Tracker():
                 'EndDeliverySupplierName':EndDeliverySupplierName,
                 'TrackingNumber':TrackingNumber,
                 'history':history}
-    '''history example,just one element in the list 
-    {'TrackID': '30629116', 'ItemID': '1478963', 'TrackStatusCode': 'CP', 
-    'TrackDate': '2018-09-27 11:52:07', 'TrackLocation': '中国港口', 
-    'TrackContent': '清关中', 'CreateDate': '2018-09-27 10:55:49',
-    'UserIDCreate': '0', 'SyncFPX': '1'}'''
+    '''history example '''
+    def track(self,reference):
+        urlpars=self.get_url(reference)
+        HTMLdict=self.get_HTML(urlpars)
+        result = self.get_keyinfo(HTMLdict)
+        return result

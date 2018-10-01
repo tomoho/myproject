b33d12d7e71cf2b420a50f686eb0fc4e61b570b7
tomoho <40868553+tomoho@users.noreply.github.com>
2018-10-01 11:51:29 -0400

    Update conf.py

--- myproject/track/conf.py
+++ myproject/track/conf.py
@@ -15,9 +15,19 @@ Url ={'天马':'http://www.worldcps.com/Order/Track?TrackNo=',
       '四方':'http://us.transrush.com/track/search.json'
      }
 milestone={
-          '天马':['订单创建','包裹入库','包裹出库','航班起飞','正在清关','国内配送'],
-          '四方':['订单创建'，]
+          '天马':['订单创建','包裹出库','航班起飞','正在清关','国内配送'],
+          '四方':['PU','LO','FT']
 }
+
+'''
+TrackStatusCode -----> FT      =========> Flight Transport 
+TrackStatusCode -----> LO      =========> Leave Overseas 
+TrackStatusCode -----> OC      =========> Overseas warehouse Change  
+TrackStatusCode -----> AO      =========> Arrive Oveseas warehouse
+TrackStatusCode -----> PU      =========> Pick Up 
+'''
 Universial_milestone =['订单创建','包裹出库','航班起飞','正在清关','国内配送']
+
+
 
 

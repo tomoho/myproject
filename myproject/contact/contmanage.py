6d8d0e9e3bdb1ddbdb47979e288b2a24cc0516eb
tomoho <40868553+tomoho@users.noreply.github.com>
2018-10-01 12:21:16 -0400

    Update contmanage.py

--- myproject/contact/contmanage.py
+++ myproject/contact/contmanage.py
@@ -1,1 +1,25 @@
-sd
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+conf file location  Staticfile.get('contact')
+'''
+Class Contact(self):
+  def __init__(self):
+    self.name=''
+    self.ID=''
+    self.cell=''
+    self.wechat=''
+    self.address=[]
+  @property  self.name,self.ID,self.address,self.cell
+  
+  
+  
+def loadcontact(fileaddress):
+  # input contact.text file address to load file 
+  with open('fileaddress','r',encod='utf8') as f:
+    for line in f:
+      contactinfo=f.readline()
+      name,ID,cell,wechat,*address=contactinfo.strip('\n').split('\t')
+  
+  

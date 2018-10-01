d61c3b197e35a03d8ddaade295c6478c0afdf567
tomoho <40868553+tomoho@users.noreply.github.com>
2018-10-01 10:51:02 -0400

    Update conf.py

--- myproject/track/conf.py
+++ myproject/track/conf.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import os,platform
 '''
 承运商数据定义在static文件中
 '''
 import logging
 logging.basicConfig(filename='logger.log',level=logging.CRITICAL)
-Version     ='0.0'
-Date    ='20180829'
+VERSION     ='0.0'
+DATE    = '20180829'
+OS =platform.system
+TIMEOUT = (10,60)
+USER_AGENT = ''
 
 Universial_milestone =['订单创建','包裹出库','航班起飞','正在清关','国内配送']
 milestone={
           '天马':['订单创建','包裹入库']
 }
+

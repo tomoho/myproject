af756c350fb6bd35989628ef4300b41fd87df141
tomoho <40868553+tomoho@users.noreply.github.com>
2018-10-01 14:19:06 -0400

    Update contact.py

--- myproject/contact/contact.py
+++ myproject/contact/contact.py
@@ -10,19 +10,25 @@ Class Contact(self):
     self.ID=''
     self.cell=''
     self.wechat=''
-    self.address=[]                         #
-  @property  self.name,self.ID,self.address,self.cell
-  
- 
-contacts=[]
-  
+    self.address=[]                   # primary address will be defined by index   
+
+    
+contacts=[]                           # 定义载入的通讯录
+fileaddress='contact.txt'  
 def loadcontact(fileaddress):
   # input contact.text file address to load file 
-  with open('fileaddress','r',encod='utf8') as f:
+  with open(fileaddress,'r',encoding='utf8') as f:
     for line in f:
-      contactinfo=f.readline()
-      name,ID,cell,wechat,*address=contactinfo.strip('\n').split('\t')
-      contacts.append(Contact(name,ID,cell,wechat,*address))
-      
+      x=Contact()
+      x.name,x.ID,x.cell,x.wechat,*x.address=contactinfo.strip('\n').split('\t')
+      contacts.append(x)
+  f.close()
+def appendcontact(fileaddress,contact):
+  with open(fileaddress,'a+',encoding='utf8') as f:
+    line='{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(contact.name,contact.ID,contact.cell,contact.wechat,contact.address)
+    f.write()
+    f.flush()
+  f.close()
+  
   
   

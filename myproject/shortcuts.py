9edda9e5ede09f7eecf5ffa4b7f567156eda1290
tomoho <40868553+tomoho@users.noreply.github.com>
2018-10-23 15:56:09 -0400

    Update shortcuts.py

--- myproject/shortcuts.py
+++ myproject/shortcuts.py
@@ -1,1 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
+# define shortcuts for command
+# import configuation
+from . import track as track
+from . import staticfileoperation as stop
+from . import message as msg
+
+# contact operation 
+LOADCONTACT=stop.loadcontact()
+ADDCONTACT = stop.addcontact()
+FINDCONTACT = stop.findcontact()
+
+#tracking operation
+LOADTRACKING =stop.loadtracking()
+LOADTRACKED = stop.loadtracked()
+FINDTRACKING = stop.findtracking()
+FINDTRACKED = stop.findtracked()
+FINDTRACK = stop.findtrack()
+
+#msg queue
+LOADMSG=stop.loadmsg()
+ADDMSG=stop.addmsg()
+CLEARMSG=stop.clearmsg()
